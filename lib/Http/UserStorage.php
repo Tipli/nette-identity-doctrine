891c6518b57b72bb12b449ef3dd966b3eb4df90a
jirikrenek <jirikrenek@users.noreply.github.com>
2022-06-23 12:40:24 +0200

    Update UserStorage.php

--- lib/Http/UserStorage.php
+++ lib/Http/UserStorage.php
@@ -51,7 +51,7 @@ class UserStorage extends NetteUserStorage
 	 * Returns current user identity, if any.
 	 * @return IIdentity|NULL
 	 */
-	public function getIdentity(): ?Nette\Security\IIdentity
+	public function getIdentity(): ?\Nette\Security\IIdentity
 	{
 		$identity = parent::getIdentity();
 
